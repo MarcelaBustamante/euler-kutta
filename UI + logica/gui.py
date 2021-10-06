3319529f7188feead2eb6e61151784129b3c9e3b
mbustamante <mbustamante@medallia.com>
2021-10-05 21:40:58 -0300

    logica H y N

--- UI + logica/gui.py
+++ UI + logica/gui.py
@@ -47,8 +47,8 @@ def imprimirVariable(equ, tcero, tene, xcero,hon,opcion):
 
 #Variables
 equ = tk.StringVar()
-tcero = tk.IntVar()
-tene = tk.IntVar()
+tcero = tk.DoubleVar()
+tene = tk.DoubleVar()
 xcero = tk.DoubleVar()
 hon = tk.DoubleVar()
 
@@ -105,18 +105,19 @@ radio2.place(x=160,y=356)
 #Campo 5
 entry_image_5 = tk.PhotoImage(file=relative_to_assets("entry_5.png"))
 entry_bg_5 = canvas.create_image(127.0,404.0,image=entry_image_5)
-entry_5 = tk.Entry(bd=0,bg="#FFFFFF",highlightthickness=0,textvariable=hon)
+entry_5 = tk.Entry(bd=0,bg="#FFFFFF",fg="#109DFA",highlightthickness=0,textvariable=hon)
 entry_5.place(x=29.0,y=384.0+19,width=196.0,height=15.0)
 canvas.create_text(25.0,389.0,anchor="nw",text="Valor N o H",fill="#5C5050",font=("RedHatDisplay Medium", 12 * -1))
 
 #Boton
 button_image_1 = tk.PhotoImage(file=relative_to_assets("button_1.png"))
 #button_1 = tk.Button(image=button_image_1,borderwidth=0,highlightthickness=0,command=lambda: imprimirVariable(equ,tcero,tene,xcero,hon,opcion),relief="flat")
-button_1 = tk.Button(image=button_image_1,borderwidth=0,highlightthickness=0,command=lambda: plot(tcero.get(),tene.get(),xcero.get()),relief="flat")
+button_1 = tk.Button(image=button_image_1,borderwidth=0,highlightthickness=0,command=lambda: plot(tcero.get(),tene.get(),xcero.get(), opcion.get(),hon.get()),relief="flat")
 button_1.place(x=38.0,y=444.0,width=170.0,height=36.0)
 
 canvas.create_rectangle(8.0,33.0,52.0,37.0,fill="#FFFFFF",outline="")
 canvas.create_rectangle(8.0,132.0,52.0,136.0,fill="#FFFFFF",outline="")
+ 
  #funcion problema   
 def f(t,x):
     #y = (50*t**2-10*x)/3
@@ -126,10 +127,9 @@ def f(t,x):
 
     return (y)
 
-def plot(tcero,tene,xcero):
-    #evaluo euler y eulerMej
-    (t,x) = euler(f, tcero, tene, xcero, 50)
-    (u,y) = eulerMej(f, tcero, tene, xcero, 50)
+def plot(tcero, tene, xcero, opcion,hon):
+    #calcular
+    (t, x, u,y) = calcular(tcero, tene, xcero, opcion, hon)
     #Creción de grafico
     fig = Figure(figsize=(5,4),dpi=100) #dpi - dots per inch
     grafico = fig.add_subplot(111)
@@ -151,19 +151,34 @@ def plot(tcero,tene,xcero):
     """
 
 """SECCION DE CALCULOS"""
+def calcular(tcero,tene,xcero,opcion,hon):
+    if opcion == 2:
+        h = (tene - tcero)/hon
+        n = int(hon)
+        print('H:', type(h).__name__)
+        print('N:', type(n).__name__)
+    else:
+        h = hon
+        n = int((tene - tcero)/hon)
+        print('H:', type(h).__name__)
+        print('N:', type(n).__name__)
+    #evaluo euler y eulerMej
+    (t,x) = euler(f, tcero, tene, xcero, h, n)
+    (u,y) = eulerMej(f, tcero, tene, xcero, h, n)
+    return (t,x,u,y)
 
 #euler
-def euler(f, t0, tf, x0, n):
+def euler(f, t0, tf, x0, h, n):
     t = np.linspace(t0, tf, n+1)
     x = np.zeros(n+1)
     x[0] = x0
-    h = (tf - t0)/n
+    
     for i in range(1, n+1):
         x[i]= x[i-1] + h * f(t[i-1],x[i-1])
     return ((t,x))
 
 #euler mejorado
-def eulerMej(f, t0, tf, x0, n):
+def eulerMej(f, t0, tf, x0, h, n):
     t = np.linspace(t0, tf, n+1)
     x = np.zeros(n+1)
     predictor = np.zeros(n+1)
@@ -174,20 +189,6 @@ def eulerMej(f, t0, tf, x0, n):
         predictor[i]= x[i-1] + h * f(t[i-1],x[i-1])
         x[i]= x[i-1] + (h/2) * (f(t[i-1],x[i-1]) + f(t[i],predictor[i]))
     return ((t,x))
-
-
- # ejemplo de una funcion problema   
-#def f(t,x):
-     #y = (50*t**2-10*x)/3
-     #y = eval('a(50*t**2-10*x)/3')
-   #  y = 0.5*(-x+t**2+4*t-1)
-     #y = -2*x  #0, 1, 1, 50
-     #y = x-t
-    # return (y)
-#200 iteraciones
-#(t,x) = euler(f, 0, 1, 1, 100)
-#(u,y) = eulerMej(f, 0, 1, 1, 50)
-
 
 window.resizable(False, False)
 window.mainloop()
